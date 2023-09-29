c52e083406285818670f3cd5e89a2025f447d599
adrianhajdin <adrianhajdin00@gmail.com>
2023-09-29 16:22:19 +0200

    fix build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,10 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300; // 5 minutes
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
+
 export async function GET() {
   try {
     connectToDB();
@@ -34,7 +38,7 @@ export async function GET() {
         }
   
         const updatedProduct = await Product.findOneAndUpdate(
-          { url: scrapedProduct.url },
+          { url: product.url },
           product,
         );
 

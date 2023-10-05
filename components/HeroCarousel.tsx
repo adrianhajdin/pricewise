38c7098fae90775fc8b05d1f321c8e1c535a6f35
Ankit Kumar <108288022+Ankitkr2021@users.noreply.github.com>
2023-10-05 17:29:45 +0530

    Added Transition on HeroCarousel.tsx
    Applied a transition for heroimage so that it could get transitioned after every 5 seconds.

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -1,25 +1,38 @@
 "use client"
 
+import { useEffect, useState } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
 import Image from "next/image";
 
 const heroImages = [
-  { imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch'},
-  { imgUrl: '/assets/images/hero-2.svg', alt: 'bag'},
-  { imgUrl: '/assets/images/hero-3.svg', alt: 'lamp'},
-  { imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer'},
-  { imgUrl: '/assets/images/hero-5.svg', alt: 'chair'},
-]
+  { imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch' },
+  { imgUrl: '/assets/images/hero-2.svg', alt: 'bag' },
+  { imgUrl: '/assets/images/hero-3.svg', alt: 'lamp' },
+  { imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer' },
+  { imgUrl: '/assets/images/hero-5.svg', alt: 'chair' },
+];
 
 const HeroCarousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Increment the current index, looping back to the first image if necessary
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % heroImages.length);
+    }, 5000); // 5000 milliseconds = 5 seconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="hero-carousel">
       <Carousel
         showThumbs={false}
-        // autoPlay
+        selectedItem={currentIndex}
         infiniteLoop
-        // interval={2000}
         showArrows={false}
         showStatus={false}
       >
@@ -46,4 +59,4 @@ const HeroCarousel = () => {
   )
 }
 
-export default HeroCarousel
+export default HeroCarousel;
